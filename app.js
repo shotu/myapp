e26de5ca6520417b1d3a47f1da4afca35b2cff12
shotu <atri.manish.iiita@gmail.com>
2015-03-30 14:34:52 +0530

    Controller basics

--- app.js
+++ app.js
@@ -5,5 +5,14 @@ var App = Ember.Application.create({
 App.Router.map(function(){
 
 	this.route('about');
+	this.resource('products');
 
+});
+
+App.IndexController = Ember.Controller.extend({
+	productsCount: 10,
+	logo: 'images/logo.png',
+	time: function(){
+		return (new Date()).toDateString()
+	}.property()
 });
