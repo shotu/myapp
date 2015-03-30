937855584c0dc307df3347d436e3cc08499b525c
shotu <atri.manish.iiita@gmail.com>
2015-03-30 16:10:30 +0530

    Controller basics

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ App.Router.map(function(){
 
 	this.route('about');
 	this.resource('products');
+	this.resource('product',{ path: '/products/:title'}); 
 
 });
 
@@ -15,4 +16,38 @@ App.IndexController = Ember.Controller.extend({
 	time: function(){
 		return (new Date()).toDateString()
 	}.property()
+});
+
+App.PRODUCTS = [
+	{
+		title:'Flint',
+		price:99,
+		description:'Flint is ...',
+		isOnSale: true,
+		image: 'images/products/flint.png'
+	},
+	{
+		title: 'Kindling',
+		price:249,
+		description: 'Easily....',
+		isOnSale: true,
+		image: 'images/products/kindling.png'
+	}
+];
+
+App.ProductsRoute = Ember.Route.extend({
+	 
+model: function(){
+	return App.PRODUCTS;
+
+}
+
+});
+
+App.ProductRoute = Ember.Route.extend({
+	model:function(params){
+		return App.PRODUCTS.findBy('title',params.title);
+	}
+
+
 });
